925d25b927ec8aa911f0deea40ef567d152c33ba
Dylan <dylanyeowf@gmail.com>
2026-03-04 17:53:02 +0800

    test

---
+++ dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,35 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-CK5a_syT.js\")",
+  "clientReferenceDeps": {
+    "f29e6e234fea": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CLoEoxvu.js",
+        "/assets/framework-Ce_Irb5n.js",
+        "/assets/index-CK5a_syT.js"
+      ],
+      "css": []
+    },
+    "0deffcb8ffd7": {
+      "js": [
+        "/assets/facade__virtual_vinext-rsc-entry-CLoEoxvu.js",
+        "/assets/framework-Ce_Irb5n.js",
+        "/assets/index-CK5a_syT.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "src/app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-D8l-U0Vn.css"
+      ]
+    },
+    "src/app/page.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-D8l-U0Vn.css"
+      ]
+    }
+  }
+}
